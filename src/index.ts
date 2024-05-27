48065b7b38fd4a4e9ec9665f0534a83cecc10fd1
Guantong <i@gt.email>
2024-05-27 18:02:25 +0800

    Ponder mainnets

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,15 @@ import { ponder } from "@/generated";
 
 const listenRelayer = [
   "0xaC2b224c2E1eD2E8663097a361A05a72d6671C7D",
-  "0xaBFEBd81169de74f5a9Dbd7C1b5E349E835852E8", // TRediXQ8qcTqorMQqJ2jQgqnjUojWZo2qc
+  "0xDD9622309aa2798D74CD39C772D289dfe3EEdaD5", // TWArBv4oRtVE4MAkqaEVHHiBQX1Wc7xDg6
 ];
 const listenOracle = [
   "0x3f938756ceFa33665719Eb528E581FF3f460b7C6",
-  "0x488116286B253D61FFd41F4948c288c3C672918F", // TGaaHxjof9QcfKwggnFVM3QwjGKzbuNzNk
+  "0xf7A4217c2c372E50c19fdF68D86b3C7E493d2d21", // TYYcXSzzc8r4Q17xrYUtqWEMtKUNwNictu
 ];
 const listenSignature = [
   "0xA72d283015c01807bc0788Bf22C1A774bDbFC8fA",
-  "0x841B6b2F3148131Ac161d88edFb2C11F146e189F", // TN1j3Ttt1c1mB3X2zdKkdMsUK6pZyLCxSr
+  "0x152c6DdDD0A4cfD817af7Cf4cf5491D4AC44e886", // TBuAR5bP2KoJ6Thx4zFqGChSARNRYrknTD
 ];
 
 ponder.on("ORMPV2:MessageAccepted", async ({ event, context }) => {
