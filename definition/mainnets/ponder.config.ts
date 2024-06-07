cf275891efc7881b812baa3ac73f8e546de389e8
Guantong <i@gt.email>
2024-06-07 09:55:01 +0800

    restore start block

--- definition/mainnets/ponder.config.ts
+++ definition/mainnets/ponder.config.ts
@@ -68,28 +68,28 @@ export default createConfig({
       address: "0x13b2211a7cA45Db2808F6dB05557ce5347e3634e",
       network: {
         arbitrum: {
-          startBlock: 218870631,
+          startBlock: 217891600,
         },
         blast: {
-          startBlock: 4416712,
+          startBlock: 4293849,
         },
         crab: {
-          startBlock: 2917100,
+          startBlock: 2900604,
         },
         darwinia: {
-          startBlock: 2849900,
+          startBlock: 2830100,
         },
         ethereum: {
-          startBlock: 20030000,
+          startBlock: 20009605,
         },
         moonbeam: {
-          startBlock: 6314163,
+          startBlock: 6294138,
         },
         polygon: {
-          startBlock: 57819999,
+          startBlock: 57710686,
         },
         tron: {
-          startBlock: 62333124,
+          startBlock: 62251337,
           address: "0x5C5c383FEbE62F377F8c0eA1de97F2a2Ba102e98", // TJPZeFEdc4TBEcNbku5xVZLQ6B2Q1oGnd1
         },
       },
