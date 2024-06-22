fec7c3ef47c25f93e28b2360786dfc7a2c25e49d
Guantong <i@gt.email>
2024-06-22 12:20:10 +0800

    update polygon rpc

--- definition/mainnets/ponder.polygon.ts
+++ definition/mainnets/ponder.polygon.ts
@@ -14,8 +14,9 @@ export default createConfig({
       chainId: 137,
       // transport: http(`https://polygon-mainnet.blastapi.io/${BLAST_API_KEY}`),
       transport: loadBalance([
-        http(`https://polygon-mainnet.blastapi.io/${BLAST_API_KEY}`),
+        // http(`https://polygon-mainnet.blastapi.io/${BLAST_API_KEY}`),
         // http(`https://hrpc.darwinia.network/polygon`),
+        http(`https://polygon-mainnet.infura.io/v3/${INFURA_API_KEY}`)
       ]),
       maxRequestsPerSecond: FAST_MAX_REQUESTS_PER_SECOND,
     },
