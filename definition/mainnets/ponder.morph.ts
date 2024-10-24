da25dbbbe6633f7a9596d58061d5bc6fffd2d926
fewensa <37804932+fewensa@users.noreply.github.com>
2024-10-24 10:46:59 +0800

    Add morph (#54)
    * Add morph
    
    * Update start block

---
+++ definition/mainnets/ponder.morph.ts
@@ -0,0 +1,42 @@
+import { createConfig, loadBalance } from "@ponder/core";
+import { http } from "viem";
+
+import { ORMPAbi as ORMPAbiV2 } from "./abis/v2/ORMPAbi";
+import { SignaturePubAbi } from "./abis/v2/SignaturePubAbi";
+
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+const BLAST_API_KEY = process.env.BLAST_API_KEY;
+const MAX_REQUESTS_PER_SECOND = 5;
+const FAST_MAX_REQUESTS_PER_SECOND = 20;
+
+export default createConfig({
+  networks: {
+    morph: {
+      chainId: 2818,
+      transport: loadBalance([
+        http("https://rpc.morphl2.io"),
+      ]),
+      maxRequestsPerSecond: FAST_MAX_REQUESTS_PER_SECOND,
+    },
+  },
+  contracts: {
+    // === V2
+    ORMPV2: {
+      abi: ORMPAbiV2,
+      address: "0x13b2211a7cA45Db2808F6dB05557ce5347e3634e",
+      network: {
+        morph: {
+          startBlock: 59565,
+        },
+      },
+      filter: {
+        event: [
+          "MessageAccepted",
+          "MessageDispatched",
+          "MessageAssigned",
+          "HashImported",
+        ],
+      },
+    },
+  },
+});
