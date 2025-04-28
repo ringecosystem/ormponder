a2e0753dfd5686a9862d090d46a7486324691644
fewensa <osuni@protonmail.com>
2025-04-28 07:13:14 +0000

    Refactor hello endpoint response and comment out unused SQL client middleware

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,14 +1,14 @@
 import { db } from "ponder:api";
 import schema from "ponder:schema";
 import { Hono } from "hono";
-import { client, graphql } from "ponder";
+import { graphql } from "ponder";
 
 const app = new Hono();
 
 app.get("/hello", (c) => {
-  return c.text("Hello, world!");
+  return c.text("hello");
 });
 app.use("/graphql", graphql({ db, schema }));
-app.use("/sql/*", client({ db, schema }));
+// app.use("/sql/*", client({ db, schema }));
 
 export default app;
